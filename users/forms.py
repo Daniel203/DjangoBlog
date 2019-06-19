212d312be08983bee17925e399cf487e28b3e55f
Daniel Arduini <daniel.arduini@4ServiceSol.loc>
2019-06-19 16:56:36 +0200

    Created side bar for mobile view

--- users/forms.py
+++ users/forms.py
@@ -5,9 +5,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    nome = forms.CharField(max_length=30, required=False)
-    cognome = forms.CharField(max_length=30, required=False)
 
+    def __init__(self, *args, **kwargs):
+            super(UserCreateForm, self).__init__(*args, **kwargs)
+            self.fields['username'].help_text = 'Massimo 150 caratteri. Non può contenere caratteri speciali.'
+            self.fields['email'].help_text = "Inserire un'email valida."
+            self.fields['password1'].help_text = 'La password deve contenere almeno 8 caratteri.'
+            self.fields['password2'].help_text = 'Inserire la stessa password'
+        
     class Meta:
         model = User
-        fields = ("username","nome", "cognome", "email", "password1", "password2")
+        fields = ("username", "email", "password1", "password2")
